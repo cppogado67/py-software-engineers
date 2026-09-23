8aa883b21613d0bb32b3f9e84c2eceff62b59e66
claro <c_ppogado67@yahoo.com>
2026-09-23 15:54:02 +0800

    solve 00

--- app/main.py
+++ app/main.py
@@ -1,1 +1,17 @@
-# write your code here
+class SoftwareEngineer:
+    def __init__(self, name):
+        self.name = name
+        self.skills = []  
+    
+    def learn_skill(self, skill):
+        self.skills.append(skill)
+
+class FrontendDeveloper(SoftwareEngineer):
+    def __init__(self, name):
+        super().__init__(name)  # calls parent's __init__
+        self.skills.extend(["JavaScript", "HTML", "CSS"])
+    
+    def create_awesome_web_page(self):
+        print(f"{self.name} is creating a webpage...")
+        return "<h1>Hello world</h1>"
+
